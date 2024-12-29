4cbb5443de1e0a33cae86a15f1e9e45e587105cb
YunYouJun <me@yunyoujun.cn>
2024-12-29 18:49:26 +0800

    chore: fix lint

--- src/modules/gtag.ts
+++ src/modules/gtag.ts
@@ -1,6 +1,6 @@
+import type { UserModule } from '~/types'
 import VueGtag from 'vue-gtag-next'
 import config from '~/config'
-import type { UserModule } from '~/types'
 
 export const install: UserModule = ({ app }) => {
   app.use(VueGtag, {
